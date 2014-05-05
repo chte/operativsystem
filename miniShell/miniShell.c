70f316cc818a0d817b72e61f959aab25199d1159
Carl Eriksson <carle293@hotmail.com>
2014-05-05 13:52:42 +0200

    Apped input from user

--- miniShell/miniShell.c
+++ miniShell/miniShell.c
@@ -1,30 +1,52 @@
 #include <stdlib.h>
 #include <unistd.h>
-#include  <stdio.h>
-#include  <sys/types.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>       /* definierar bland annat WIFEXITED */
+#include <stdio.h>			/* för kunna använda fget för indata från användaren */
 
 int main(int argc, char **argv) {
 
-	pid_t  pid;
+	char word[70];
+	int count = 1;
 
-	pid = fork();
-    if (pid == 0){
+	/* Ta emot indata från användaren */
+	while (fgets(word, sizeof(word), stdin) != NULL){
+        count = strlen(word);
+        word[--count] = '\0'; /*discard the newline character*/
+        printf("Command to execute: %s %d\n",  word, count);
 
-    	/* Create the command */
-     	char* cmdLs[] = {"ls",NULL};
-
-     	execvp("ls", cmdLs);
-     	/*printf ("The value returned was: %d.\n",i);*/
-     	printf("Unknown command\n");
-
-    	exit(0);
+        if(strcmp(word,"exit") == 0){
+       	 	printf("User want to exit, exit now\n");
+       	 	break;
+    	}
+    	else {
+    		executeForeGround(word);
+    	}
     }
-    else{
-     	int status = 0;
-     	wait(&status);
-     	printf ("Child process is returned with: %d.\n",status);
-    }
-
 
      exit(0);
- }
+}
+
+void executeForeGround(char *cmd){
+	pid_t pid;
+	pid = fork();
+	if (pid == 0){
+
+    	/* Create the command */
+		char* cmdLs[] = {&cmd,NULL};
+
+		execvp("ls", cmdLs);
+     	/*printf ("The value returned was: %d.\n",i);*/
+		printf("Unknown command\n");
+
+		exit(0);
+	}
+	else{
+		int status = 0;
+		int childId = wait(&status);
+		printf ("Child process(id %d) is returned with status: %d.\n",childId,status);
+	}
+
+	return;
+}
